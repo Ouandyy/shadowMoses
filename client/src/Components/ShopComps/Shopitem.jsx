1c1983f58b4892d8aa867f06ff91bf816e7fd0f0
Ouandyy <ouandyy@gmail.com>
2019-09-19 17:32:16 -0700

    pushing shop items

--- client/src/Components/ShopComps/Shopitem.jsx
+++ client/src/Components/ShopComps/Shopitem.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
-const item = (props) => {
-  return (
-    <div className="itemContainer">
-    <div className="itemImg">
-    <img src={props.item.preview1}></img>
-    <div>{props.item.name}</div>
-    <div>${props.item.price}</div>
-    </div>
-    </div>
-  )
+class Item extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    photo: this.props.item.preview1,
+    photo2: this.props.item.preview2
+  }
+  this.onHover = this.onHover.bind(this);
+  this.leaveHover = this.leaveHover.bind(this); 
 }
 
-export default item
+onHover (e) {
+  e.target.src = this.state.photo2
+}
+
+leaveHover (e) {
+  e.target.src = this.state.photo
+}
+
+
+  render () {
+    return (
+    <div className="itemContainer">
+    <div className="itemImg">
+    <img src={this.state.photo} onMouseEnter={this.onHover} onMouseLeave={this.leaveHover}></img>
+    <div>{this.props.item.name}</div>
+    <div>${this.props.item.price}</div>
+    </div>
+    </div>
+  )}
+}
+  
+
+
+export default Item
